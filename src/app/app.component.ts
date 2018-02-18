ef17a748b6cfdf6550970189d37864c8d4a9b550
hruday kumar reddy <hrudaykumar11@gmail.com>
2018-02-18 14:42:34 +0530

    testing1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CommonServiceService } from './services/common-service.service';
+import { TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public modalRef: BsModalRef;
+  keys: string[]=[]; 
+  public deletedData : any;
+  public viewData : any;
+  public updatedIdData: any
+  public viewId : string;
+
+  public openModal(template: TemplateRef<any>, id) {
+    this.modalRef = this.modalService.show(template);
+    this.viewId = id;
+  }
+  constructor(private commonService: CommonServiceService, private modalService: BsModalService){}
+  public data :any;
+  ngOnInit(){
+  	this.commonService.dynamicTable().subscribe(tableData => {
+  		console.log(tableData);
+  		this.data = tableData;
+      this.keys= Object.keys(this.data[0]);
+  	})
+  }
+  updateData=()=>{
+
+    this.commonService.updateData(this.viewId).subscribe(res=>{
+      this.updatedIdData = res;
+      console.log(this.updatedIdData);
+    })
+  }
+  viewDetails=(id)=>{
+    this.commonService.viewData(id).subscribe(res=>{
+      this.viewData = res;
+      alert(JSON.stringify(this.viewData));
+    })
+  }
+   deleteData=(id)=>{
+    this.commonService.deleteData(id).subscribe(res =>{
+      this.deletedData = res;
+      console.log(this.deletedData);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+import { ButtonsModule } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+
+import { CommonServiceService } from './services/common-service.service';
 
 
 @NgModule({
@@ -10,9 +18,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, 
+    FormsModule,
+    ButtonsModule.forRoot(),
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [ CommonServiceService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
